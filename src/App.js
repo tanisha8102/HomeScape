208a75df6c186c14fa6652bc563e47f83de47bcc
tanisha8102 <tanisha8102@gmail.com>
2025-01-14 01:33:32 +0530

    removed contact

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutUs from './pages/About-us'
-import ContactPage from './pages/Contact'
 import FAQPage from './pages/FAQ'
 import Product from './pages/Product'
 
@@ -13,7 +12,6 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/contact" element={<ContactPage />} />
           <Route path="/faq" element={<FAQPage />} />
           <Route path="/product" element={<Product />} />
           
